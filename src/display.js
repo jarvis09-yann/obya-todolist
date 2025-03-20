324a27bdb4f0d5e5673b9ac2b8eedbe8b4c6a1b9
jarvis09 <contact@jarvis09.com>
2025-03-20 12:33:29 +0100

    fixed displaycontroller and added some more methods

--- src/display.js
+++ src/display.js
@@ -1,11 +1,15 @@
-export class displayController {
+const todolistContainer = document.querySelector(".todolist-container");
+
+export class DisplayController {
   constructor() {}
 
-  static renderTodoItem(obj) {
+  static renderItem(obj) {
     const todoItem = document.createElement("div");
     todoItem.classList.add("todolist-item");
     todolistContainer.appendChild(todoItem);
-
+    const todoCheckbox = document.createElement("input");
+    todoCheckbox.setAttribute("type", "checkbox");
+    todoItem.appendChild(todoCheckbox);
     const todoTitle = document.createElement("h2");
     todoTitle.textContent = obj.title;
     todoItem.appendChild(todoTitle);
@@ -22,21 +26,42 @@ export class displayController {
     switch (obj.priority) {
       case 1:
         priorityButton.classList.add("priority-1");
+        break;
       case 2:
         priorityButton.classList.add("priority-2");
+        break;
       case 3:
         priorityButton.classList.add("priority-3");
+        break;
+      default:
+        priorityButton.classList.add("priority-1");
+        break;
     }
+    todoButtonContainer.appendChild(priorityButton);
+
     const editIcon = document.createElement("box-icon");
     editIcon.setAttribute("type", "solid");
     editIcon.setAttribute("name", "edit");
     editIcon.setAttribute("color", "#405d72");
     editIcon.setAttribute("class", "edit");
+    todoButtonContainer.appendChild(editIcon);
 
     const deleteIcon = document.createElement("box-icon");
     deleteIcon.setAttribute("type", "solid");
     deleteIcon.setAttribute("name", "trash-alt");
     deleteIcon.setAttribute("color", "darkred");
     deleteIcon.setAttribute("class", "delete");
+    todoButtonContainer.appendChild(deleteIcon);
+  }
+
+  static clear() {
+    todolistContainer.innerHTML = "";
+  }
+
+  static renderAll(array) {
+    this.clear();
+    for (let element in array) {
+      DisplayController.renderItem(array[element]);
+    }
   }
 }
