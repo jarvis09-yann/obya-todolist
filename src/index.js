ffe0337fc424c02c7ed8e2e5592b9b7ae7e30b8d
jarvis09 <contact@jarvis09.com>
2025-03-21 08:54:06 +0100

    worked on the todolist rendering logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./style.css";
 import "./todos.js";
+import "./display.js";
+import { TodoList } from "./todos.js";
+TodoList.load();
+
+import { DisplayController } from "./display.js";
 
 const sidebarCollapse = document.querySelector("#sidebar-collapse");
 const todolistContainer = document.querySelector(".todolist-container");
@@ -37,3 +42,16 @@ aside {
     sidebarCollapse.innerHTML = "";
   }
 });
+
+// for (let i = 0; i <= 100; i++) {
+//   TodoList.add(
+//     `Item ${i}`,
+//     "Lorem Ipsum dolor sit amet",
+//     false,
+//     new Date("March 17 2025 13:30"),
+//     3
+//   );
+// }
+
+DisplayController.setCategory(TodoList.get(0, "default"));
+DisplayController.renderAll();
