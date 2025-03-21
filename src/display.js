c96e74d6344e65dd604b15b0eba939d56d3efa35
jarvis09 <contact@jarvis09.com>
2025-03-21 08:53:16 +0100

    added logic for action buttons

--- src/display.js
+++ src/display.js
@@ -1,15 +1,32 @@
+import { TodoList } from "./todos";
+
 const todolistContainer = document.querySelector(".todolist-container");
 
 export class DisplayController {
+  static #category = "default";
   constructor() {}
-
   static renderItem(obj) {
     const todoItem = document.createElement("div");
     todoItem.classList.add("todolist-item");
     todolistContainer.appendChild(todoItem);
+
     const todoCheckbox = document.createElement("input");
     todoCheckbox.setAttribute("type", "checkbox");
     todoItem.appendChild(todoCheckbox);
+
+    todoCheckbox.addEventListener("click", () => {
+      if (todoCheckbox.checked) {
+        TodoList.set(obj.id, "isDone", true);
+        todoCheckbox.parentElement.style.textDecoration = "line-through";
+        todoCheckbox.parentElement.style.color = "gray";
+      } else {
+        TodoList.set(obj.id, "isDone", false);
+        todoCheckbox.parentElement.style.textDecoration = "none";
+        todoCheckbox.parentElement.style.color = "inherit";
+      }
+      console.log(TodoList.getAll());
+    });
+
     const todoTitle = document.createElement("h2");
     todoTitle.textContent = obj.title;
     todoItem.appendChild(todoTitle);
@@ -19,6 +36,8 @@ export class DisplayController {
     todoItem.appendChild(todoContent);
 
     const todoButtonContainer = document.createElement("div");
+    todoButtonContainer.classList.add("action-buttons");
+    todoButtonContainer.id = obj.id;
     todoItem.appendChild(todoButtonContainer);
 
     const priorityButton = document.createElement("div");
@@ -39,29 +58,56 @@ export class DisplayController {
     }
     todoButtonContainer.appendChild(priorityButton);
 
-    const editIcon = document.createElement("box-icon");
-    editIcon.setAttribute("type", "solid");
-    editIcon.setAttribute("name", "edit");
-    editIcon.setAttribute("color", "#405d72");
-    editIcon.setAttribute("class", "edit");
+    const editIcon = document.createElement("div");
+    editIcon.innerHTML =
+      '<box-icon type="solid" name="edit" color="#405d72" class="edit"></box-icon>';
     todoButtonContainer.appendChild(editIcon);
 
-    const deleteIcon = document.createElement("box-icon");
-    deleteIcon.setAttribute("type", "solid");
-    deleteIcon.setAttribute("name", "trash-alt");
-    deleteIcon.setAttribute("color", "darkred");
-    deleteIcon.setAttribute("class", "delete");
+    const deleteIcon = document.createElement("div");
+    deleteIcon.innerHTML =
+      '<box-icon type="solid" name="trash-alt" color="darkred" class="delete"></box-icon>';
     todoButtonContainer.appendChild(deleteIcon);
+
+    todoButtonContainer.addEventListener("click", (e) => {
+      if (e.target.classList.contains("edit")) {
+        console.log("edit");
+        console.log(e.target.parentElement.parentElement.id);
+      } else if (e.target.classList.contains("delete")) {
+        TodoList.remove(e.target.parentElement.parentElement.id);
+      } else if (e.target.classList.contains("priority")) {
+        updatePriority(e.target.parentElement.id, e.target.classList[1]);
+      }
+      this.renderAll();
+    });
   }
 
   static clear() {
     todolistContainer.innerHTML = "";
   }
 
-  static renderAll(array) {
+  static setCategory(category) {
+    this.#category = category;
+  }
+
+  static renderAll() {
     this.clear();
-    for (let element in array) {
-      DisplayController.renderItem(array[element]);
+    for (let element in this.#category) {
+      DisplayController.renderItem(this.#category[element]);
     }
   }
+}
+
+function updatePriority(id, CurrentPriority) {
+  switch (CurrentPriority) {
+    case "priority-1":
+      TodoList.set(id, "priority", 2);
+      break;
+    case "priority-2":
+      TodoList.set(id, "priority", 3);
+      break;
+    case "priority-3":
+      TodoList.set(id, "priority", 1);
+      break;
+  }
+  DisplayController.renderAll();
 }
